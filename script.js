2fd248c1e4d0ec0e8932e29751697fdad63fefcb
Lunirs <dhong0925@gmail.com>
2022-05-11 22:47:50 -0400

    Added if statements to validate user choices

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-\// Assignment Code
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -9,60 +9,86 @@ function writePassword() {
   passwordText.value = password;
 }
 
-// use split to split string into array
-//combine use .concat()
+//Array to make string into Array to create the following
+// list of lower case characters
+// list of upper case characters
+// list of special characters
+// list of numbers
+
 var lower = "abcdefghijklmnopqrstuvwxyz";
 var specialChar = "!@#$%^&*()_+-=";
 var numbers = "0123456789";
+var lowerArray = Array(lower);
+var upperArray = Array(lower.toUpperCase());
+var specialCharArray = Array(specialChar);
+var numberArray = Array(numbers);
 
-  // list of special characters
-  // list of numbers
-  // list of upper case characters
-  // list of lower case characters
+//create blank arrays
 
-var lowerArray = lower.split("");
-var upperArray = lower.toUpperCase().split("");
-var specialCharArray = specialChar.split("");
-var numberArray = numbers.split("");
+var generatePassword = function () {
+  var password = "";
+  var finalArr = "";
+  var confirmLength = "";
 
-function generatePassword() {
+  //prompt for confirming the length of your password
 
-  var password = ""
-  var finalArr = ""
-  var confirmLength = ""
-
-  confirmLength = prompt.(`Please specify the number of characters you would like for your password.
+  confirmLength =
+    prompt(`Please specify the number of characters you would like for your password.
   Password must be 8-128 characters.`);
 
-  while(confirmLength <=7 || confirmLength >= 129) {
-    alert.(`Password length must be 8-128 characters. 
-    Please try again.`)
-    if(confirmLength === null) {
+  //while loop to confirm user's choice was between 8 and 128
+
+  while (confirmLength <= 7 || confirmLength >= 129) {
+    alert(`Password length must be 8-128 characters. 
+    Please try again.`);
+
+    confirmLength =
+      prompt(`Please specify the number of characters you would like for your password.
+  Password must be 8-128 characters.`);
+
+    // if user cancels or does not put in anything it returns  an alert
+
+    if (confirmLength === null) {
       alert("Please enter the number of characters");
       return;
     }
   }
 
-  var confirmLowalph = confirm("Would you like for your password to include lower case letters?");
-  var confirmUpAlph = confirm("Would you like for your password to include capital letters?");
-  var confirmSpecchar = confirm("Would you like for your password to include special characters?");
-  var confirmNumm = confirm("Would you like for your password to include numbers?");
+  //confirmations to add certain characters
 
+  var confirmLowalph = confirm(
+    "Would you like for your password to include lower case letters?"
+  );
+  var confirmUpAlph = confirm(
+    "Would you like for your password to include capital letters?"
+  );
+  var confirmSpecchar = confirm(
+    "Would you like for your password to include special characters?"
+  );
+  var confirmNum = confirm(
+    "Would you like for your password to include numbers?"
+  );
 
-  // how many characters?
-  // is it 8 or more characters?
-  // no? send an error message
-  // use special characters?
-  // use numbers?
-  // use capital letters?
-  // use lower case letters?
+  //combine final array with conditional arrays using concat based on user's choice
 
-  // put together password
-  // combine the arrays that were chosen yes to
-  // pick random characters from combined array based on how many characters there are
+  if (confirmLowalph) {
+    finalArr = finalArr.concat(lowerArray);
+  }
 
-  return "password";
-}
+  if (confirmUpAlph) {
+    finalArr = finalArr.concat(upperArray);
+  }
+
+  if (confirmSpecchar) {
+    finalArr = finalArr.concat(specialCharArray);
+  }
+
+  if (confirmNum) {
+    finalArr = finalArr.concat(numberArray);
+  }
+
+  return password;
+};
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
